14631c29757750a701ddaa0f56bd41483298792d
= <=>
2021-04-15 17:20:03 +1000

    updated to 0.1.10

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ author = 'Zhong Dai'
 
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.8'
+release = '0.1.10'
 
 
 # -- General configuration ---------------------------------------------------
