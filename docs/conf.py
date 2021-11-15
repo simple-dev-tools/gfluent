974cf947b2771b6a0af122cbfd90e88beb0b7204
Zhong Dai <zhdai@woolworths.com.au>
2021-11-15 12:48:59 +1100

    bump version to 0.1.16

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ author = 'Simple Dev Tools'
 
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.14'
+release = '0.1.16'
 
 
 # -- General configuration ---------------------------------------------------
